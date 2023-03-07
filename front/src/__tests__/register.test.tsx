d7de0bb12e95df3a73d04f0b866ae7056a206a0c
Benavente Matías <benaventematias04@gmail.com>
2023-03-07 17:49:36 -0300

    render the component separate in each test

--- front/src/__tests__/register.test.tsx
+++ front/src/__tests__/register.test.tsx
@@ -1,10 +1,17 @@
 import '@testing-library/jest-dom';
-import '@testing-library/react/dont-cleanup-after-each';
 
 import { MockedProvider } from '@apollo/client/testing';
-import { cleanup, fireEvent, render, waitFor } from '@testing-library/react';
+import {
+  cleanup,
+  fireEvent,
+  render,
+  waitFor,
+  waitForElementToBeRemoved,
+} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { ZodError } from 'zod';
 
 import { mockedUseRegisterMutation } from '@/__tests__/__mocks__/mockedApolloHooks';
 import { mockLocalStorage } from '@/__tests__/__mocks__/mockedLocalStorage';
@@ -15,7 +22,7 @@ import { registerFields } from '@/utils/registerUtils';
 
 const { setItemMock } = mockLocalStorage();
 
-const mocks = [
+export const mocks = [
   {
     request: {
       query: RegisterDocument,
@@ -35,21 +42,41 @@ const mocks = [
 ];
 
 describe('Register form test render and works just fine with the correct data being sent', () => {
-  afterAll(() => {
+  afterEach(() => {
     cleanup();
   });
 
-  const wrapper = render(
-    <BrowserRouter>
-      <Provider store={store}>
-        <MockedProvider mocks={mocks} addTypename={false}>
-          <Register />
-        </MockedProvider>
-      </Provider>
-    </BrowserRouter>,
-  );
+  test('should render each component needed in register correctly', async () => {
+    const view = render(
+      <BrowserRouter>
+        <Provider store={store}>
+          <MockedProvider mocks={mocks} addTypename={false}>
+            <Register />
+          </MockedProvider>
+        </Provider>
+      </BrowserRouter>,
+    );
 
-  it('should fill the inputs and send data', async () => {
+    registerFields.forEach((field) => {
+      expect(
+        view.container.querySelector(`input[name="${field.name}"]`),
+      ).toBeInTheDocument();
+    });
+    expect(view.queryByTestId('send-btn')).toBeInTheDocument();
+    expect(view.queryByTestId('send-btn')).toBeDisabled();
+  });
+
+  test('should fill the inputs and send data', async () => {
+    const view = render(
+      <BrowserRouter>
+        <Provider store={store}>
+          <MockedProvider mocks={mocks} addTypename={false}>
+            <Register />
+          </MockedProvider>
+        </Provider>
+      </BrowserRouter>,
+    );
+
     type IMockedData = {
       [key: string]: string;
     };
@@ -63,28 +90,68 @@ describe('Register form test render and works just fine with the correct data be
     };
 
     registerFields.forEach(async (field) => {
-      const input = wrapper.container.querySelector(
-        `input[name="${field.name}"]`,
-      ) as Element;
-      fireEvent.change(input, { target: { value: mockedData[`${field.name}`] } });
+      const input = view.container.querySelector(`input[name="${field.name}"]`);
+      fireEvent.change(input as Element, {
+        target: { value: mockedData[`${field.name}`] },
+      });
     });
 
     await waitFor(() => {
       registerFields.forEach(async (field) => {
-        const input = wrapper.container.querySelector(
+        const input = view.container.querySelector(
           `input[name="${field.name}"]`,
         ) as Element;
 
         expect(input).toHaveValue(mockedData[`${field.name}`]);
-        expect(wrapper.queryByTestId('send-btn')).not.toBeDisabled();
+        expect(view.queryByTestId('send-btn')).not.toBeDisabled();
       });
     });
 
-    fireEvent.click(wrapper.queryByTestId('send-btn') as Element);
+    fireEvent.click(view.queryByTestId('send-btn') as Element);
 
     await waitFor(() => {
       expect(mockedUseRegisterMutation).toHaveBeenCalled();
       expect(setItemMock).toHaveBeenCalled();
+    });
+  });
+
+  test('should show a popover showing the user what input validation is missing', async () => {
+    const view = render(
+      <BrowserRouter>
+        <Provider store={store}>
+          <MockedProvider mocks={mocks} addTypename={false}>
+            <Register />
+          </MockedProvider>
+        </Provider>
+      </BrowserRouter>,
+    );
+    let popover: Element = document.body;
+    registerFields.forEach(async (field) => {
+      const input = view.container.querySelector(`input[name="${field.name}"]`);
+      userEvent.click(input as Element);
+
+      await waitFor(() => {
+        popover = view.getByTestId('validation-popover');
+      });
+
+      //Get the validation errors by parsing with empty string
+      let validationTest: ZodError<any> = new ZodError([]);
+      try {
+        field.validation.validation?.parse('');
+      } catch (err: any) {
+        validationTest = err;
+      }
+
+      //Test if validation errors are being shown in a popover
+      validationTest.issues.forEach(async (issue) => {
+        expect(view.getByTestId('validation-popover')).toHaveTextContent(issue.message);
+      });
+
+      //The close popover in real UI is called by clicking anywhere in the page
+      //as Popover generates this component using the hole window
+      userEvent.click(popover);
+
+      waitForElementToBeRemoved(() => popover);
     });
   });
 });
